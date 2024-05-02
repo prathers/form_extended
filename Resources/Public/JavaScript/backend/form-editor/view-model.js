814fae540ef5974b83bca6408879cde02df98e38
Sven Wappler <wappler@wappler.systems>
2024-05-02 02:09:23 +0200

    updates

--- Resources/Public/JavaScript/backend/form-editor/view-model.js
+++ Resources/Public/JavaScript/backend/form-editor/view-model.js
@@ -3,6 +3,9 @@
  */
 import $ from 'jquery';
 import * as Helper from '@typo3/form/backend/form-editor/helper.js'
+import Modal from '@typo3/backend/modal.js';
+import Severity from '@typo3/backend/severity.js';
+import Icons from '@typo3/backend/icons.js';
 
 /**
  * @private
@@ -95,6 +98,26 @@ function _subscribeEvents() {
   getPublisherSubscriber().subscribe('view/inspector/editor/insert/perform', function(topic, args) {
     _renderEditorDispatcherFormExtended(args[0],args[1],args[2],args[3]);
   });
+  getPublisherSubscriber().subscribe('view/stage/abstract/button/openRichtextModal/clicked', function(topic, args) {
+
+
+
+
+        Modal.show(
+          'ddddddddddd',
+          'frfrfrrfrr',
+          Severity.info
+        );
+
+
+  });
+
+  /*
+  getPublisherSubscriber().subscribe('view/inspector/editor/insert/perform', function(topic, args) {
+    _addListeners();
+  });*/
+
+
 };
 
 /**
@@ -118,6 +141,7 @@ function _renderTemplateDispatcherFormExtended(formElement, template) {
  */
 function _renderEditorDispatcherFormExtended(editorConfiguration, editorHtml, collectionElementIdentifier, collectionName) {
   //console.debug(editorConfiguration['templateName']);
+
   switch (editorConfiguration['templateName']) {
     case 'Inspector-StaticTextEditor':
       renderStaticTextEditor(
@@ -127,8 +151,28 @@ function _renderEditorDispatcherFormExtended(editorConfiguration, editorHtml, co
         collectionName
       );
       break;
+    case 'Inspector-ModalTextareaEditor':
+      renderModalTextareaEditor(
+        editorConfiguration,
+        editorHtml,
+        collectionElementIdentifier,
+        collectionName
+      );
+      break;
   }
 };
+
+function _updateConfiguration() {
+
+  getHelper().setConfiguration({
+    domElementDataAttributeValues: {
+      buttonStageRichtextEditor: 'stageRichtextEditor'
+    }
+  });
+
+
+
+}
 
 /**
  * @public
@@ -138,8 +182,11 @@ function _renderEditorDispatcherFormExtended(editorConfiguration, editorHtml, co
  */
 export function bootstrap(formEditorApp) {
   _formEditorApp = formEditorApp;
+  _updateConfiguration();
   _helperSetup();
   _subscribeEvents();
+
+
 };
 
 
@@ -207,6 +254,125 @@ function renderStaticTextEditor(editorConfiguration, editorHtml, collectionEleme
 
     getHelper().getTemplatePropertyDomElement('propertyPath', editorHtml).text(propertyData);
   }
+
+
+
+
+  return;
+  if (
+    !getUtility().isUndefinedOrNull(editorConfiguration['additionalElementPropertyPaths'])
+    && 'array' === $.type(editorConfiguration['additionalElementPropertyPaths'])
+  ) {
+    for (var i = 0, len = editorConfiguration['additionalElementPropertyPaths'].length; i < len; ++i) {
+      getCurrentlySelectedFormElement().set(editorConfiguration['additionalElementPropertyPaths'][i], propertyData);
+    }
+  }
+
+  renderFormElementSelectorEditorAddition(editorConfiguration, editorHtml, propertyPath);
+
+  getHelper().getTemplatePropertyDomElement('propertyPath', editorHtml).on('keyup paste', function() {
+    if (
+      !!editorConfiguration['doNotSetIfPropertyValueIsEmpty']
+      && !getUtility().isNonEmptyString($(this).val())
+    ) {
+      getCurrentlySelectedFormElement().unset(propertyPath);
+    } else {
+      getCurrentlySelectedFormElement().set(propertyPath, $(this).val());
+    }
+    _validateCollectionElement(propertyPath, editorHtml);
+    if (
+      !getUtility().isUndefinedOrNull(editorConfiguration['additionalElementPropertyPaths'])
+      && 'array' === $.type(editorConfiguration['additionalElementPropertyPaths'])
+    ) {
+      for (var i = 0, len = editorConfiguration['additionalElementPropertyPaths'].length; i < len; ++i) {
+        if (
+          !!editorConfiguration['doNotSetIfPropertyValueIsEmpty']
+          && !getUtility().isNonEmptyString($(this).val())
+        ) {
+          getCurrentlySelectedFormElement().unset(editorConfiguration['additionalElementPropertyPaths'][i]);
+        } else {
+          getCurrentlySelectedFormElement().set(editorConfiguration['additionalElementPropertyPaths'][i], $(this).val());
+        }
+      }
+    }
+  });
+};
+
+/**
+ * @public
+ *
+ * @param object editorConfiguration
+ * @param object editorHtml
+ * @param string collectionElementIdentifier
+ * @param string collectionName
+ * @return void
+ * @throws 1475421053
+ * @throws 1475421054
+ * @throws 1475421055
+ * @throws 1475421056
+ */
+function renderModalTextareaEditor(editorConfiguration, editorHtml, collectionElementIdentifier, collectionName) {
+  var propertyData, propertyPath;
+  assert(
+    'object' === $.type(editorConfiguration),
+    'Invalid parameter "editorConfiguration"',
+    1475421053
+  );
+  assert(
+    'object' === $.type(editorHtml),
+    'Invalid parameter "editorHtml"',
+    1475421054
+  );
+  assert(
+    getUtility().isNonEmptyString(editorConfiguration['label']),
+    'Invalid configuration "label"',
+    1475421055
+  );
+
+
+  getHelper()
+    .getTemplatePropertyDomElement('label', editorHtml)
+    .append(editorConfiguration['label']);
+  if (getUtility().isNonEmptyString(editorConfiguration['fieldExplanationText'])) {
+    getHelper()
+      .getTemplatePropertyDomElement('fieldExplanationText', editorHtml)
+      .text(editorConfiguration['fieldExplanationText']);
+  } else {
+    getHelper()
+      .getTemplatePropertyDomElement('fieldExplanationText', editorHtml)
+      .remove();
+  }
+
+  if (getUtility().isNonEmptyString(editorConfiguration['placeholder'])) {
+    getHelper()
+      .getTemplatePropertyDomElement('propertyPath', editorHtml)
+      .attr('placeholder', editorConfiguration['placeholder']);
+  }
+
+  if (editorConfiguration['propertyPath']  !== undefined) {
+    propertyPath = getFormEditorApp().buildPropertyPath(
+      editorConfiguration['propertyPath'],
+      collectionElementIdentifier,
+      collectionName
+    );
+    propertyData = getCurrentlySelectedFormElement().get(propertyPath);
+
+    //getFormEditorApp().getInspector().getEditor().getValidator()._validateCollectionElement(propertyPath, editorHtml);
+    //_validateCollectionElement(propertyPath, editorHtml);
+
+    getHelper().getTemplatePropertyDomElement('propertyPath', editorHtml).text(propertyData);
+  }
+
+  getHelper().getTemplatePropertyDomElement('stageRichtextEditor', editorHtml).on('click', function(e) {
+    getPublisherSubscriber().publish(
+      'view/stage/abstract/button/openRichtextModal/clicked', [
+        'view/insertElements/perform/bottom'
+      ]
+    );
+  });
+
+
+
 
   return;
   if (
